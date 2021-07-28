4d2c84124e61232f19e901f17d78ecdc32f618dc
joshiejack <joshjackwildman@gmail.com>
2021-07-28 12:09:30 +0100

    Remove unused classes

--- src/main/java/uk/joshiejack/gastronomy/plugins/shopaholic/ListingBuilderCooked.java
+++
@@ -1,9 +0,0 @@
-package uk.joshiejack.gastronomy.plugins.shopaholic;
-
-//@PenguinLoader("cooked")
-//public class ListingBuilderCooked extends ListingBuilder<ItemStack> {
-//    @Override
-//    public List<String> items() {
-//        return Recipe.RECIPE_BY_STACK.keySet().stream().filter(k -> !k.getStacks().get(0).isEmpty()).map(key -> StackHelper.getStringFromStack(key.getStacks().get(0))).collect(Collectors.toList());
-//    }
-//}
